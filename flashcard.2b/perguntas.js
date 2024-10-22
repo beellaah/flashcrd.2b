6d0d32884d8791310f730ed66f05454eca9dc944
beellaah <140733133+beellaah@users.noreply.github.com>
2024-10-22 12:08:54 -0300

    Update perguntas.js

--- flashcard.2b/perguntas.js
+++ flashcard.2b/perguntas.js
@@ -1,1 +1,12 @@
+criaCartao(
+    'Biologia',
+    'O que são vacinas? ',
+    'Produtos biológicos que estimulam a defesa do corpo contra alguns microorganismo que provocam doenças'
+)
+
+criaCartao(
+    'Empreendedorismo',
+  'Qual o conceito de escabilidade',
+    'Capacidade que uma empresa tem de sustentar ou de melhorar seu desempenho'
+)
 
